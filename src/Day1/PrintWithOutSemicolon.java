1a4be0b4d4c634649034d26dcc47b57805060500
Mehmet Buyukertas <acizkul6327@gmail.com>
2021-03-30 07:44:18 +0200

    Create new Framework

---
+++ src/Day1/PrintWithOutSemicolon.java
@@ -0,0 +1,28 @@
+package Day1;
+
+public class PrintWithOutSemicolon {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+/*
+        if( System.out.append("Hello World")  != null  ){
+        }
+*/
+
+        /*
+        if( System.out.printf("Hello")  == null  ){
+        }
+         */
+
+        while( System.out.append("Hello World")  == null){
+
+        }
+
+
+
+
+
+    }
+
+}
